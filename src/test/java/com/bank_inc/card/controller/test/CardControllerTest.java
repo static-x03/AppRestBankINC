ba83e64cb7f08ea34da13907b5c82ed91b1cbe0e
Bryan Vanegas A. <36496978+static-x03@users.noreply.github.com>
2025-02-08 17:56:15 -0500

    skip testing

--- src/test/java/com/bank_inc/card/controller/test/CardControllerTest.java
+++ src/test/java/com/bank_inc/card/controller/test/CardControllerTest.java
@@ -14,7 +14,6 @@ import java.math.BigDecimal;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -30,7 +29,6 @@ public class CardControllerTest {
 	@Autowired
 	private MockMvc mockMvc;
 
-	@MockBean
 	private CardService cardService;
 
 	@Test
--- src/test/java/com/bank_inc/card/controller/test/TransactionControllerTest.java
+++ src/test/java/com/bank_inc/card/controller/test/TransactionControllerTest.java
@@ -10,7 +10,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -24,7 +23,6 @@ public class TransactionControllerTest {
 	@Autowired
 	private MockMvc mockMvc;
 
-	@MockBean
 	private TransactionService transactionService;
 
 	@Test
